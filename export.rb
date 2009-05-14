147dc399c78cd0c625fe79a008ed5dde5f2b1cea
chenillen <chenillen@gmail.com>
2009-05-14 18:23:46 +0800

    split func added

--- export.rb
+++ export.rb
@@ -6,16 +6,37 @@ CSV_FILE_PATH = File.join(File.dirname(__FILE__), "data/data.csv")
 
 
 # read the file
+# code_map = <<END_CODE
+# Case/card # cols 1-4
+# Card Start Width Items Total Variable
+# No.  Col.              Cols.
+# END_CODE         
 
 FasterCSV.foreach(CSV_FILE_PATH, :headers => true, :header_converters => :symbol) do |line|
-  for n in 1..15
-   p line[n]       
+  ascii = line[:id] + '   '
+  code_map = "" 
+  start_col = 6
+  item = 1  
+  for n in 1..16
+   if line[n]
+     
+     line[n].split("|").each{ |unit| ascii += unit}
+     
+     
+     ascii += line[n]
+     start_col += line[n-1].length * item unless n == 1
+     item  if line[n].split("|" ,)
+     #code_map += line[:id]  +"    " + start_col.to_s + "     " + line[n].length.to_s + "    " + item.to_s + line[:header] + '\n' 
+   end      
   end
- 
-   p line[:id] + "      " + line[:q1] + line[:q2] + line[:q3] + line[:q4] + line[:q5] + line[:q6]
+  p ascii
+  p code_map
+  p start_col
   
-end 
- 
+end   
+
+
+
 # DATA = File.read(CSV_FILE_PATH)
 # 
 # table = FCSV.parse(DATA, :headers => true, :header_converters => :symbol)
